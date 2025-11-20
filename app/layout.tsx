05153f011d8819483b1c04f05f9d9a2e970cfab5
dulce303 <dulce303@gmail.com>
2025-11-20 12:53:59 -0800

    Build #18: Add Google Analytics tracking
    - Added Google tag (gtag.js) with ID G-13LJT5ZV5Z
    - Tracking code added to root layout for all pages
    - Will track page views and user interactions
    - Bundle size: 1.09 MB (stable)

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={notoSans.variable}>
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-13LJT5ZV5Z"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-13LJT5ZV5Z');
+            `,
+          }}
+        />
+      </head>
       <body className="antialiased relative font-sans">
         <ServiceWorkerRegistration />
         <ClientErrorHandler />
